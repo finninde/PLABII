05c679f9a4160422d8a05a0701178708a6fe4421
n35n0m <n35n0m@gmail.com>
2015-10-13 01:55:20 +0200

    Inclusion of a main method. To be used in program testing/program run on demonstration. Doesn't do anything fancy atm.

---
+++ main.py
@@ -0,0 +1,5 @@
+from imager import Imager
+
+if __name__ == "__main__":
+    theTest = Imager(width=100) ##Creates a default instant of the Imager wrapper class, which contains a black 100x100 square
+    theTest.display()
