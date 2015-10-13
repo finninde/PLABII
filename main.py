35762a89aecd11ed979c8531e94ec3a372a67b79
n35n0m <n35n0m@gmail.com>
2015-10-13 02:10:40 +0200

    Finishing up for the day (no major changes), checking setup on both PCs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from imager import Imager
+from imager import ptest2
 
 if __name__ == "__main__":
-    theTest = Imager(width=100) ##Creates a default instant of the Imager wrapper class, which contains a black 100x100 square
-    theTest.display()
+    testImage = ptest2()    #Nothing's like a trippy Einstein-pic to get things going.
